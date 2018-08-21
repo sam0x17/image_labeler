1188a32149b16557611365fe4e418e2c042ee3eb
Sam Johnson <sam@durosoft.com>
2018-08-20 22:32:00 -0400

    add basic web server

--- image_labeler.js
+++ image_labeler.js
@@ -1,1 +1,12 @@
 const sharp = require('sharp');
+const fs = require('fs');
+const http = require('http');
+
+const index = fs.readFileSync('./index.html');
+
+console.log('started server at localhost:8080');
+
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.end(index);
+}).listen(8080);
